7ce2906d5ea6767e92f1d730c393fd49b8978c07
shahzaintariq.crpyto@gmail.com <shahzaintariq.crpyto@gmail.com>
2020-12-15 20:36:00 +0500

    done

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-const todoItemEl = document.querySelectorAll('.todo__item');
+const todoListEl = document.querySelector('.todo__list');
+const todoInputEl = document.querySelector('.todo__input');
+
+todoInputEl.addEventListener('keyup',(event) => {
+    if(event.keyCode === 13){
+        addTodo(todoInputEl.value)
+        todoInputEl.value = ''
+        deleteItem()
+    }
+})
+
+function deleteItem(){
+    const todoItemEl = document.querySelectorAll('.todo__item');
+    for(i=0; i<todoItemEl.length; i++){
+        todoItemEl[i].addEventListener('click',(event)=>{
+            event.target.className += ' done';
+        })
+    }
+}
+
+function addTodo(item){
+    todoListEl.innerHTML += `<li class="todo__item">${item}</li>`;
+}
