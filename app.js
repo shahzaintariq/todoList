40fd579b6ed67a02159f95f4975155aa564587eb
shahzaintariq.crpyto@gmail.com <shahzaintariq.crpyto@gmail.com>
2020-12-16 14:48:05 +0500

    add new method in app.s

--- app.js
+++ app.js
@@ -3,21 +3,35 @@ const todoInputEl = document.querySelector('.todo__input');
 
 todoInputEl.addEventListener('keyup',(event) => {
     if(event.keyCode === 13){
-        addTodo(todoInputEl.value)
+        todoListEl.insertBefore(addTodo(todoInputEl.value),todoListEl.childNodes[0]);
         todoInputEl.value = ''
-        deleteItem()
+        // deleteItem()
     }
 })
 
-function deleteItem(){
-    const todoItemEl = document.querySelectorAll('.todo__item');
-    for(i=0; i<todoItemEl.length; i++){
-        todoItemEl[i].addEventListener('click',(event)=>{
-            event.target.className += ' done';
-        })
-    }
+/* THIS FUNCTIONS WORKS BUT NOT A GOOD PRACTICE*/
+// function deleteItem(){
+//     const todoItemEl = document.querySelectorAll('.todo__item');
+//     for(i=0; i<todoItemEl.length; i++){
+//         todoItemEl[i].addEventListener('click',(event)=>{
+//             event.target.className += ' done';
+//         })
+//     }
+// }
+
+function toggleDone() {
+    todoListEl.addEventListener('click',(e) => {
+        if(e.target.classList.contains('todo__item')){
+            e.target.classList.toggle('done');    
+        }
+    })
 }
 
+toggleDone()
+
 function addTodo(item){
-    todoListEl.innerHTML += `<li class="todo__item">${item}</li>`;
+    const newListElem = document.createElement('li');
+    newListElem.textContent = item;
+    newListElem.setAttribute('class', 'todo__item');
+    return newListElem;
 }
